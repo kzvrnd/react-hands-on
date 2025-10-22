213e0f6e8e327f5fc956f903ae05c3c9b68ba267
kzv <kzvrnd@gmail.com>
2025-10-21 17:02:40 -0700

    used axios to fetch the data

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,14 +1,21 @@
+import axios from 'axios';
 import { Header } from "../components/Header";
 import { products } from "../../starting-code/data/products";
 import "./HomePage.css";
 
 export function HomePage() {
 
-  fetch('http://localhost:3000/api/products')
+  //Does the same as axios below, but alot cleaner
+  // fetch('http://localhost:3000/api/products')
+  //   .then((response) => {
+  //     return response.json()
+  //   }).then((data) =>{
+  //       console.log(data);
+  //   });
+
+  axios.get('http://localhost:3000/api/products')
     .then((response) => {
-      return response.json()
-    }).then((data) =>{
-        console.log(data);
+      console.log(response.data);
     });
 
   return (
